11e0d4b39779625a34a4afcec58016cac7e1dd53
Cijisource <Cijai4u@gmail.com>
2020-10-02 18:17:53 +0530

    Implemented Sintex

--- NodeMCU_02/NodeMCU_02.ino
+++ NodeMCU_02/NodeMCU_02.ino
@@ -1,3 +1,8 @@
+#include <SoftwareSerial.h>
+SoftwareSerial serialPort(D1,D0);
+#include <ArduinoJson.h>
+
+#define BLYNK_PRINT Serial  
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
@@ -12,26 +17,98 @@ char auth[] = "ODbXgkyA-fZohqppkwa0qm8QusGnDXCa";
 //char ssid[] = "Cijaiz complex";
 //char pass[] = "9000150001";
 
+BlynkTimer timer;
+long systemUptime, uptimesec;
+long distance, cdistance;
+float tankPercentage, ctankPercentage, compressorTankPercentage, cementTankPercentage;
+float availableLitres, cavailableLitres; 
+float consumedLitres, cconsumedLitres, waterlevelat;
+
 bool isSTankLowEmailSent = false;
 bool isSTankFullEmailSent = false;
 bool isCTankLowEmailSent = false;
 bool isCTankFullEmailSent = false;
 
-float compressorTankPercentage = 0;
-float cementTankPercentage = 0;
-WidgetLED led0(V1);
-WidgetLED led1(V2);
-WidgetLED led2(V3);
-WidgetLED led3(V4);
-
-BlynkTimer timer;
-
 void setup() {
+  Serial.println("----------------SETUP INITIATED--------------------------");
   Blynk.begin(auth, ssid, pass);
-  
-  Serial.begin(115200);
   // Setup a function to be called every second
   timer.setInterval(1000L, uploadtoBlynk);
+  
+  Serial.begin(9600);
+  serialPort.begin(115200);
+  while (!Serial) continue;
+  Serial.println("----------------SETUP COMPLETED--------------------------");
+}
+
+void ExtractSensorData() {
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(serialPort);
+
+  long uptimemls = millis();
+  uptimesec = uptimemls/1000;
+  
+  Serial.println("Esp uptime ");
+  Serial.println(uptimesec);
+
+  if (!serialPort.available() > 0) {
+    //Serial.println("received Data");
+    distance = -100;
+//    Serial.println("Didnt Receive Data");
+  }  
+  
+  if(root == JsonObject::invalid()) 
+    return;
+
+  Serial.println("JSON Received and Parsed");
+  root.prettyPrintTo(Serial);
+  Serial.println("");
+
+  systemUptime=root["ArduinoUptime"];
+  distance=root["SSensorDistance"];
+  tankPercentage=root["STankLevelPercentage"];
+  
+  availableLitres = root["SAvailableLitres"];
+  consumedLitres = root["SConsumedLitres"];
+  waterlevelat = root["SWaterlevel"];
+        
+  //Serial.println("ArduinoUptime ");
+  //Serial.print(systemUptime);
+  //Serial.println("");
+  
+  //Serial.println("SensorDistance ");
+  //Serial.print(distance);
+  //Serial.println("");
+
+  //Serial.println("TankLevelPercentage ");
+  //Serial.print(tankPercentage);
+  //Serial.println("");
+
+  //Serial.println("AvailableLitres ");
+  //Serial.print(availableLitres);
+  //Serial.println("");
+
+  //Serial.println("ConsumedLitres ");
+  //Serial.print(consumedLitres);
+  //Serial.println("");
+  
+  //Serial.println("----------------------");
+}
+
+void uploadtoBlynk(){
+  Blynk.virtualWrite(V4, tankPercentage);
+  Blynk.virtualWrite(V1, distance);
+  Blynk.virtualWrite(V2, consumedLitres);
+  Blynk.virtualWrite(V3, availableLitres);
+  
+  Blynk.virtualWrite(V5, systemUptime);  
+  Blynk.virtualWrite(V6, uptimesec);
+
+  Blynk.virtualWrite(V7, compressorTankPercentage);
+  Serial.println(compressorTankPercentage);
+
+  Blynk.virtualWrite(V8, cementTankPercentage);
+  Serial.println(cementTankPercentage);
 }
 
 BLYNK_CONNECTED(){
@@ -93,40 +170,8 @@ BLYNK_WRITE(V10) {
   }
 }
 
-void uploadtoBlynk(){
-  Blynk.virtualWrite(V0, compressorTankPercentage);
-  Serial.println(compressorTankPercentage);
-
-  Blynk.virtualWrite(V10, cementTankPercentage);
-  Serial.println(cementTankPercentage);
-
-  if(compressorTankPercentage < 25) {
-    led0.on();
-    led1.off();
-    led2.off();
-    led3.off();
-  }
-  if(compressorTankPercentage > 25 && compressorTankPercentage < 50) {
-    led0.on();
-    led1.on();
-    led2.off();
-    led3.off();
-  }
-  if(compressorTankPercentage > 50 && compressorTankPercentage < 75) {
-    led0.on();
-    led1.on();
-    led2.on();
-    led3.off();
-  }
-  if(compressorTankPercentage > 75 && compressorTankPercentage <= 100) {
-    led0.on();
-    led1.on();
-    led2.on();
-    led3.on();
- }
-}
-
 void loop() {
+  ExtractSensorData();
   Blynk.run();
   timer.run(); // Initiates SimpleTimer
 }
